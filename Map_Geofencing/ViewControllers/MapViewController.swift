8085a1a9940302ea31c08b3795ea6acf074c7390
Yeontae Kim <ytk007@gmail.com>
2017-12-22 21:43:16 -0800

    Add segmented control for changing map types

--- Map_Geofencing/ViewControllers/MapViewController.swift
+++ Map_Geofencing/ViewControllers/MapViewController.swift
@@ -11,7 +11,7 @@ import GoogleMaps
 import CoreLocation
 
 class MapViewController: UIViewController {
-    
+
     var photoStore: PhotoStore!
     var fetchedPlants = [Plant]()
     var locationManager = CLLocationManager()
@@ -28,15 +28,23 @@ class MapViewController: UIViewController {
         // Google Maps setting
         let camera = GMSCameraPosition.camera(withLatitude: 37.7669, longitude: -122.4716, zoom: 15.0)
         mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.mapType = .normal
         mapView.isMyLocationEnabled = true
         self.view = mapView
         self.mapView.delegate = self
         
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: 37.7669, longitude: -122.4716)
-        marker.title = "Tree Name"
-        marker.snippet = "common name"
-        marker.map = mapView
+        // Initialize Segmented Control
+        let items = ["Standard", "Hybrid", "Satellite"]
+        let segmentedControl = UISegmentedControl(items: items)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        // Set up SegmentedControl
+        segmentedControl.frame = CGRect(x: 90, y: 75,
+                                      width: 200, height: 25)
+        segmentedControl.addTarget(self, action: #selector(mapType(sender:)), for: .valueChanged)
+        segmentedControl.backgroundColor = .white
+        segmentedControl.tintColor = .black
+        self.view.addSubview(segmentedControl)
         
         //Location Manager code to fetch current location
         setUpLocationManager()
@@ -61,8 +69,19 @@ class MapViewController: UIViewController {
         
     }
     
-    // Fetch all saved pins with annotation
+    // Change map types
+    func mapType(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .normal
+        case 1:
+            mapView.mapType = .hybrid
+        default:
+            mapView.mapType = .satellite
+        }
+    }
     
+    // Fetch all saved pins with annotation
     func fetchAllPlantMarkers() {
         
         photoStore.fetchAllMarkers() { (plantsResult) in
@@ -94,6 +113,19 @@ class MapViewController: UIViewController {
             }
         }
         
+    }
+    
+    @IBAction func mapTypeChange(_ sender: UISegmentedControl!) {
+        switch (sender.selectedSegmentIndex) {
+        case 0:
+            mapView.mapType = .normal
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            mapView.mapType = .normal
+        }
     }
 }
 
@@ -135,24 +167,26 @@ extension MapViewController: GMSMapViewDelegate {
     }
     
     func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
-        let view = UIView(frame: CGRect.init(x: 0, y: 0, width: 200, height: 50))
+        let view = UIView(frame: CGRect.init(x: 0, y: 0, width: 225, height: 50))
         view.backgroundColor = UIColor.white
-//        view.layer.cornerRadius = 25
+        view.layer.cornerRadius = 25
         
         let plantImage =  UIImageView(frame: CGRect(x: view.frame.origin.x + 2.5, y: view.frame.origin.y + 2.5, width: 45, height: 45))
         plantImage.layer.cornerRadius = 22.5
         plantImage.image = UIImage(named: "sample")
+        plantImage.clipsToBounds = true
         view.addSubview(plantImage)
         
-        let scientificNameLabel = UILabel(frame: CGRect.init(x: 58, y: 8, width: view.frame.size.width - 16, height: 15))
+        let scientificNameLabel = UILabel(frame: CGRect.init(x: 58, y: 8, width: view.frame.size.width - 16, height: 16))
         scientificNameLabel.text = marker.title
-        scientificNameLabel.font = UIFont.systemFont(ofSize: 14)
+        scientificNameLabel.font = UIFont.systemFont(ofSize: 16)
         view.addSubview(scientificNameLabel)
         
-        let distanceLabel = UILabel(frame: CGRect.init(x: 58, y: 25, width: view.frame.size.width - 16, height: 15))
+        let distanceLabel = UILabel(frame: CGRect.init(x: 58, y: 28, width: view.frame.size.width - 16, height: 14))
         if let distance = userCurrentLocation?.distance(from: CLLocation(latitude: marker.position.latitude, longitude: marker.position.longitude)) {
             distanceLabel.text = self.formatDistance(distance)
-            distanceLabel.font = UIFont.systemFont(ofSize: 12)
+            distanceLabel.font = UIFont.systemFont(ofSize: 14)
+            distanceLabel.textColor = .lightGray
         }
         view.addSubview(distanceLabel)
         
@@ -160,7 +194,6 @@ extension MapViewController: GMSMapViewDelegate {
     }
     
     // Format distance(meters) to miles in String
-    
     func formatDistance(_ distance: Double) -> String {
         
         let distanceMeters = Measurement(value: distance, unit: UnitLength.meters)
