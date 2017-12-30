5a42a21fc1f6fa184bd4dd40257c057b87861b85
Yeontae Kim <ytk007@gmail.com>
2017-12-29 23:24:29 -0800

    Implement sign in process with firebase auth

--- Map_Geofencing/ViewControllers/SignInWithEmailViewController.swift
+++ Map_Geofencing/ViewControllers/SignInWithEmailViewController.swift
@@ -7,29 +7,183 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
 
 class SignInWithEmailViewController: UIViewController {
-
+    
+    // MARK: Properties
+    let activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    var alertController: UIAlertController?
+    var keyboardOnScreen = false
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        subscribeToNotification(.UIKeyboardWillShow, selector: #selector(keyboardWillShow))
+        subscribeToNotification(.UIKeyboardWillHide, selector: #selector(keyboardWillHide))
+        subscribeToNotification(.UIKeyboardDidShow, selector: #selector(keyboardDidShow))
+        subscribeToNotification(.UIKeyboardDidHide, selector: #selector(keyboardDidHide))
+        
+        setupActivityIndicator()
+        
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupActivityIndicator() {
+        
+        // Activity Indicator Setup
+        activityIndicator.center = view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.color = UIColor.lightGray
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        view.addSubview(activityIndicator)
+        
     }
-    */
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        usernameTextField.text = ""
+        passwordTextField.text = ""
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromAllNotifications()
+    }
+    
+    // MARK: Login
+    @IBAction func loginPressed(_ sender: Any) {
+        
+        userDidTapView(self)
+        self.activityIndicator.startAnimating()
+        
+        guard let username = usernameTextField.text, let password = passwordTextField.text else {
+            self.activityIndicator.stopAnimating()
+            self.getAlertView(title: "Login Failed", error: "User Name or Password is empty!!!")
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: username, password: password) { (user, error) in
+            
+            print(username, password)
+            
+            performUIUpdatesOnMain {
+                if (user != nil) {
+                    self.activityIndicator.stopAnimating()
+                    self.completeLogin()
+                } else {
+                    self.activityIndicator.stopAnimating()
+                    self.getAlertView(title: "Login Error", error: (error?.localizedDescription)!)
+                    
+                    self.usernameTextField.text = ""
+                    self.passwordTextField.text = ""
+                }
+            }
+        }
+    }
+    
+    func completeLogin() {
+        performUIUpdatesOnMain {
+            self.dismiss(animated: false, completion: {
+                self.navigationController!.popToRootViewController(animated: true)
+            })
+        }
+    }
+    
+}
 
+// MARK: - SignInWithEmailViewController: UITextFieldDelegate
+
+extension SignInWithEmailViewController: UITextFieldDelegate {
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func keyboardWillShow(_ notification: Notification) {
+        if !keyboardOnScreen {
+            view.frame.origin.y -= keyboardHeight(notification) - 80
+        }
+    }
+    
+    func keyboardWillHide(_ notification: Notification) {
+        if keyboardOnScreen {
+            view.frame.origin.y = 0
+        }
+    }
+    
+    func keyboardDidShow(_ notification: Notification) {
+        keyboardOnScreen = true
+    }
+    
+    func keyboardDidHide(_ notification: Notification) {
+        keyboardOnScreen = false
+    }
+    
+    private func keyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = (notification as NSNotification).userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    private func resignIfFirstResponder(_ textField: UITextField) {
+        if textField.isFirstResponder {
+            textField.resignFirstResponder()
+        }
+    }
+    
+    @IBAction func userDidTapView(_ sender: AnyObject) {
+        resignIfFirstResponder(usernameTextField)
+        resignIfFirstResponder(passwordTextField)
+    }
+    
+    
+}
+
+// MARK: - SignInWithEmailViewController (Configure UI)
+
+extension SignInWithEmailViewController {
+    
+    func setUIEnabled(_ enabled: Bool) {
+        usernameTextField.isEnabled = enabled
+        passwordTextField.isEnabled = enabled
+        loginButton.isEnabled = enabled
+        
+        // adjust login button alpha
+        if enabled {
+            loginButton.alpha = 1.0
+        } else {
+            loginButton.alpha = 0.5
+        }
+    }
+
+}
+
+
+// MARK: - SignInWithEmailViewController (Notifications)
+
+private extension SignInWithEmailViewController {
+    
+    func subscribeToNotification(_ notification: NSNotification.Name, selector: Selector) {
+        NotificationCenter.default.addObserver(self, selector: selector, name: notification, object: nil)
+    }
+    
+    func unsubscribeFromAllNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
 }
