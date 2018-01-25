4693884cab26c4550219919e9311668e693d2bd4
Yeontae Kim <ytk007@gmail.com>
2018-01-24 17:09:53 -0800

    Refactoring app delegate

--- Map_Geofencing/AppDelegate.swift
+++ Map_Geofencing/AppDelegate.swift
@@ -22,7 +22,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     lazy var  photoStore = PhotoStore(modelName: "SFBG")
-    var locationManager:CLLocationManager? = .none
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
@@ -37,13 +36,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Provide core data with hard coded plants data
         importJSONSeedDataIfNeeded()
-        
-        // Request Authorization for User Location Access
-        performUIUpdatesOnMain {
-            self.locationManager = CLLocationManager()
-            self.locationManager?.delegate = self
-            self.locationManager?.desiredAccuracy = kCLLocationAccuracyBest
-        }
 
         // Pass photoStore to TabBarController
         guard let tabBarController = window?.rootViewController as? TabBarController else {
@@ -51,7 +43,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         tabBarController.photoStore = photoStore
-        tabBarController.locationManager = locationManager
         
         // Setup styles - status bar, nav bar, item
         Appearance.setGlobalAppearance()
@@ -141,140 +132,3 @@ extension AppDelegate {
     
 }
 
-extension AppDelegate: UNUserNotificationCenterDelegate {
-
-    // MARK: - Region monitoring event handler
-    
-    // The geofencing related codes below refers to the solution from
-    // https://www.raywenderlich.com/136165/core-location-geofencing-tutorial
-    
-    //  The notification related codes below refers to the solution from
-    // https://stackoverflow.com/questions/39103095/unnotificationattachment-with-uiimage-or-remote-url
-    // and https://useyourloaf.com/blog/local-notifications-with-ios-10/
-    
-    func handleEvent(forRegion region: CLRegion!, plant: Plant) {
-        
-        // Create notification contents
-        let content = UNMutableNotificationContent()
-        content.title = "Nearby Plant"
-        content.subtitle = plant.scientificName!
-        content.body = "Find your plant nearby"
-        
-        let openAction = UNNotificationAction(identifier:"open",
-                                              title:"Open",options:[])
-        
-        let category = UNNotificationCategory(identifier: "actionCategory",
-                                              actions: [openAction],
-                                              intentIdentifiers: [], options: [])
-        
-        content.categoryIdentifier = "actionCategory"
-        
-        UNUserNotificationCenter.current().setNotificationCategories(
-            [category])
-        
-        let photos = Array(plant.photo!) as! [Photo]
-        guard let photo = photos.first else { return }
-        
-        if let attachment = UNNotificationAttachment.create(identifier: plant.scientificName!, imageData: photo.imageData! as Data, options: nil) {
-            content.attachments = [attachment]
-        }
-        
-        // Deliver the notification in one seconds.
-        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 1.0, repeats: false)
-        
-        let requestIdentifier = "plantNotification"
-        let request = UNNotificationRequest(identifier: requestIdentifier, content: content, trigger: trigger)
-        
-        UNUserNotificationCenter.current().delegate = self
-        UNUserNotificationCenter.current().add(request) { (error) in
-            
-            if (error != nil){
-                print(error?.localizedDescription ?? "something wrong")
-            }
-        }
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        
-        switch response.actionIdentifier {
-        case "open":
-            
-            // Open Detail View Controller
-            guard let tabBarController = window?.rootViewController as? TabBarController else { return }
-            
-            if let favoriteVC = tabBarController.viewControllers?[2].navigationController?.viewControllers.first {
-                
-                let storyboard = UIStoryboard (name: "Main", bundle: nil)
-                let detailVC = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
-                
-                favoriteVC.navigationController?.pushViewController(detailVC, animated: true)
-            }
-            
-        default:
-            break
-        }
-        completionHandler()
-    }
-    
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        
-        if notification.request.identifier == "plantNotification" {
-            
-            completionHandler( [.alert,.sound,.badge])
-            
-        }
-    }
-}
-
-extension AppDelegate: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        
-        print("location manager didChangeAuth called")
-        
-        switch status {
-        case .notDetermined:
-            manager.requestWhenInUseAuthorization()
-        case .authorizedWhenInUse:
-            manager.startUpdatingLocation()
-        
-            // Requesting Authorization for User Interactions only when location service granted
-            let center = UNUserNotificationCenter.current()
-            center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
-                // Enable or disable features based on authorization.
-            }
-            break
-        case .restricted:
-            // restricted by e.g. parental controls. User can't enable Location Services
-            break
-        case .denied:
-            // user denied your app access to Location Services, but can grant access from Settings.app
-            break
-        default:
-            break
-        }
-        
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        
-        if region is CLCircularRegion {
-            
-            photoStore.fetchAllPlants() { (plantsResult) in
-                
-                switch plantsResult {
-                    
-                case let .success(plants):
-                    
-                    // Get plant with specific user & get favorite plants
-                    let plant = plants.filter { $0.scientificName! == region.identifier }.first!
-                    self.handleEvent(forRegion: region, plant: plant)
-                    
-                case .failure(_):
-                    print("No plants")
-                }
-            }
-        }
-    }
-}
--- Map_Geofencing/ViewControllers/MapViewController.swift
+++ Map_Geofencing/ViewControllers/MapViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import MapKit
 import CoreLocation
+import UserNotifications
+import UserNotificationsUI
 
 class MapViewController: UIViewController {
 
@@ -16,7 +18,7 @@ class MapViewController: UIViewController {
     
     var annotations = [MKAnnotation]()
     var photoStore: PhotoStore!
-    var locationManager: CLLocationManager?
+    var locationManager: CLLocationManager!
     var fetchedPlants = [Plant]()
     var userCurrentLocation:CLLocation?
 
@@ -36,10 +38,13 @@ class MapViewController: UIViewController {
         // Get photoStore from TabBarController
         let tabBar = self.tabBarController as! TabBarController
         self.photoStore = tabBar.photoStore
-        self.locationManager = tabBar.locationManager
+        self.locationManager = CLLocationManager()
+        self.locationManager.requestWhenInUseAuthorization()
         
-        locationManager?.delegate = self
-        
+        performUIUpdatesOnMain() {
+            self.locationManager.delegate = self
+            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -250,15 +255,61 @@ extension MapViewController: MKMapViewDelegate {
 
 extension MapViewController: CLLocationManagerDelegate {
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        print("location manager didChangeAuth called")
+        mapView.showsUserLocation = status == .authorizedWhenInUse
+        
+        switch status {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+            
+            // Requesting Authorization for User Interactions only when location service granted
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+                // Enable or disable features based on authorization.
+            }
+            break
+        case .restricted:
+            // restricted by e.g. parental controls. User can't enable Location Services
+            break
+        case .denied:
+            // user denied your app access to Location Services, but can grant access from Settings.app
+            break
+        default:
+            break
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        
+        if region is CLCircularRegion {
+            
+            photoStore.fetchAllPlants() { (plantsResult) in
+                
+                switch plantsResult {
+                    
+                case let .success(plants):
+                    
+                    // Get plant with specific user & get favorite plants
+                    let plant = plants.filter { $0.scientificName! == region.identifier }.first!
+                    self.handleEvent(forRegion: region, plant: plant)
+                    
+                case .failure(_):
+                    print("No plants")
+                }
+            }
+        }
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let userLocation = locations.last
         let _ = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
 
          locationManager?.stopUpdatingLocation()
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        mapView.showsUserLocation = status == .authorizedWhenInUse
     }
     
     func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
@@ -275,6 +326,94 @@ extension MapViewController: CLLocationManagerDelegate {
         mapView.setRegion(coordinateRegion, animated: true)
     }
 
+}
+
+extension MapViewController: UNUserNotificationCenterDelegate {
+    
+    // MARK: - Region monitoring event handler
+    
+    // The geofencing related codes below refers to the solution from
+    // https://www.raywenderlich.com/136165/core-location-geofencing-tutorial
+    
+    //  The notification related codes below refers to the solution from
+    // https://stackoverflow.com/questions/39103095/unnotificationattachment-with-uiimage-or-remote-url
+    // and https://useyourloaf.com/blog/local-notifications-with-ios-10/
+    
+    func handleEvent(forRegion region: CLRegion!, plant: Plant) {
+        
+        // Create notification contents
+        let content = UNMutableNotificationContent()
+        content.title = "Nearby Plant"
+        content.subtitle = plant.scientificName!
+        content.body = "Find your plant nearby"
+        
+        let openAction = UNNotificationAction(identifier:"open",
+                                              title:"Open",options:[])
+        
+        let category = UNNotificationCategory(identifier: "actionCategory",
+                                              actions: [openAction],
+                                              intentIdentifiers: [], options: [])
+        
+        content.categoryIdentifier = "actionCategory"
+        
+        UNUserNotificationCenter.current().setNotificationCategories(
+            [category])
+        
+        let photos = Array(plant.photo!) as! [Photo]
+        guard let photo = photos.first else { return }
+        
+        if let attachment = UNNotificationAttachment.create(identifier: plant.scientificName!, imageData: photo.imageData! as Data, options: nil) {
+            content.attachments = [attachment]
+        }
+        
+        // Deliver the notification in one seconds.
+        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 1.0, repeats: false)
+        
+        let requestIdentifier = "plantNotification"
+        let request = UNNotificationRequest(identifier: requestIdentifier, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().add(request) { (error) in
+            
+            if (error != nil){
+                print(error?.localizedDescription ?? "something wrong")
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        switch response.actionIdentifier {
+        case "open":
+            
+            // Open Detail View Controller
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            
+            guard let tabBarController = appDelegate.window!.rootViewController as? TabBarController else { return }
+            
+            if let favoriteVC = tabBarController.viewControllers?[2].navigationController?.viewControllers.first {
+                
+                let storyboard = UIStoryboard (name: "Main", bundle: nil)
+                let detailVC = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+                
+                favoriteVC.navigationController?.pushViewController(detailVC, animated: true)
+            }
+            
+        default:
+            break
+        }
+        completionHandler()
+    }
+    
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        if notification.request.identifier == "plantNotification" {
+            
+            completionHandler( [.alert,.sound,.badge])
+            
+        }
+    }
 }
 
 
